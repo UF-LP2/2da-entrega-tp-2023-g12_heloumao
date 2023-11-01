9fea75000bc5f8107d92e352cbc6b63ccc27fbf7
Valentina Mao <maovalentina2004@gmail.com>
2023-10-31 22:42:58 -0300

    inicio del arbol

--- src/cArbolSintomas.py
+++ src/cArbolSintomas.py
@@ -1,1 +1,11 @@
 #class cArbolSintomas:
+class Node:
+    def __init__(self, val, left = Node, right = Node):
+        self.val=val
+        self.left=left
+        self.right=right
+    def insert(self, data):
+        
+
+        
+
--- test/test_lecturaArchivos.py
+++ test/test_lecturaArchivos.py
@@ -1,7 +1,6 @@
-import pytest
-
 from cEnfermero import cEnfermero
 from lecturaArchivos import readNurse
+import pytest
 
 def test_archivos():
     leido=readNurse()
